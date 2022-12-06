1cf1de0197d65303d08322b0ba7cd93dfc175c61
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-12-07 00:38:59 +0530

    Added support for multiple filters

--- src/components/common/ColumnFilterIcon.js
+++ src/components/common/ColumnFilterIcon.js
@@ -1,23 +1,34 @@
 import {BiSearchAlt} from "react-icons/bi";
+import {FaSearchPlus} from "react-icons/fa";
 import {AiOutlineClose} from "react-icons/ai";
 import {TiTick} from "react-icons/ti";
 import ExpandableButton from "./expandableButton";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
 export const ColumnFilterIcon = ({ column }) => {
   const { filterValue, setFilter } = column;
   const [expanded, setExpanded] = useState(false);
 
-  const clearFilter = () => {
+  const clearFilter = useCallback(() => {
     setExpanded(!expanded);
     setFilter("");
-  }
+  }, [expanded]);
+
+  const searchIcon = filterValue ?
+      <FaSearchPlus
+          onClick={e => setExpanded(!expanded)}
+          style={{cursor: "pointer"}}
+      /> :
+      <BiSearchAlt
+          onClick={e => setExpanded(!expanded)}
+          style={{cursor: "pointer"}}
+      />;
 
   return (
       <>
       <ExpandableButton
           title="S"
-          icon={<BiSearchAlt onClick={e => setExpanded(!expanded)} style={{cursor: "pointer"}}/>}
+          icon={searchIcon}
           value={expanded}
           onChange={e => setExpanded(!expanded)}
           popupPosition={{bottom:"30px", left: "-100px"}}
