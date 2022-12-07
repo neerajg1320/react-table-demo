0a53b8b383cb1bb2782acff39abd740e0bf734a1
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-12-07 16:29:15 +0530

    Supported icon flags

--- src/components/common/basic/InputWithIcons.js
+++ src/components/common/basic/InputWithIcons.js
@@ -1,43 +1,64 @@
 import {RxLetterCaseCapitalize} from "react-icons/rx";
 import {TbLetterW} from "react-icons/tb";
+import {SiExpress} from "react-icons/si";
 import {useState} from "react";
 import './inputIcons.css';
 
 const InputWithIcons = ({value, onChange, disabled}) => {
   const [caps, setCaps] = useState(false);
   const [word, setWord] = useState(false);
+  const [regex, setRegex] = useState(false);
+
+  const InputIcon = ({ children, value, onChange }) => {
+    return (
+      <span
+          className={`${value ? "active" : ""}`}
+          onClick={e => onChange(!value)}
+          style={{cursor:"pointer", border:"1px dashed lightblue", fontSize: "0.9em"}}
+      >
+        {children}
+      </span>
+    );
+  }
 
   return (
       <div
           style={{
+            position:"relative",
             display: "flex",
             flexDirection:"row",
             gap:"4px",
-            alignItems:"center"}}
+            alignItems:"start"
+          }}
       >
         <input
             disabled={disabled}
             className="form-control"
             value={value}
             onChange={onChange}
-            style={{width: "120px"}}
+            style={{width: "200px"}}
         />
 
-        <div className="filter-flags" style={{display:"flex", flexDirection:"row", gap:"4px"}}>
-          <span
-              className={`filter-caps ${caps ? "active" : ""}`}
-              onClick={e => setCaps(!caps)}
-              style={{cursor:"pointer", border:"1px dashed blue"}}
-          >
+        <div
+            className="filter-flags"
+            style={{
+              display:"flex",
+              flexDirection:"row",
+              gap:"4px",
+              // border:"1px dashed red",
+              // position: "absolute",
+              right: "5px"
+            }}
+        >
+          <InputIcon value={caps} onChange={e => setCaps(!caps)}>
             <RxLetterCaseCapitalize />
-          </span>
-          <span
-              className={`filter-word ${word ? "active" : ""}`}
-              onClick={e => setWord(!word)}
-              style={{cursor:"pointer", border:"1px dashed blue"}}
-          >
+          </InputIcon>
+          <InputIcon value={word} onChange={e => setWord(!word)}>
             <TbLetterW />
-          </span>
+          </InputIcon>
+          <InputIcon value={regex} onChange={e => setRegex(!regex)}>
+            <SiExpress />
+          </InputIcon>
         </div>
       </div>
   );
